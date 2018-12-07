f361b0d228bbd976ac04e90e7666856c836ce53a
rchen2018 <rec43@duke.edu>
2018-12-07 17:03:30 -0500

    asdf

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -72,12 +72,45 @@ public class HuffProcessor {
 	}
 	
 	public HuffNode readTreeHeader(BitInputStream in) {
-		int bit = in.read();
+		int bit = in.readBits(1);
 		if (bit == -1) {
-			//throw new Exception;
+			throw new HuffException("reading bits failed");
 		}
 		if (bit == 0) {
-			left = readTreeHeader(BitInputStream in);
+			HuffNode left = readTreeHeader(in);
+			HuffNode right = readTreeHeader(in);
+			return new HuffNode(0,0,left,right);
+		}
+		else {
+			int value = in.readBits(BITS_PER_WORD + 1);
+			return new HuffNode(value,0,null,null);
+		}
+	}
+	
+	public void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
+		HuffNode current = root;
+		while (true) {
+			int bits = in.readBits(1);
+			if (bits == -1) {
+				throw new HuffException("bad input, no PSEUDO_EOF");
+			}
+			else {
+				if (bits == 0) { 
+					current = current.myLeft;
+				}
+				else {
+					current = current.myRight;
+				}
+				if (current.myLeft == null && current.myRight == null) {
+					if (current.myValue == PSEUDO_EOF) {
+						break;
+					}
+					else {
+						out.writeBits(bits, current.myValue);
+						current = root;
+					}
+				}
+			}
 		}
 	}
 }
