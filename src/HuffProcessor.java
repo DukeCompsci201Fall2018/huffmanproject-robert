b78b8690cb61dabc90c2dd2c47a4aa29599162e2
rchen2018 <rec43@duke.edu>
2018-12-07 18:02:23 -0500

    asdf

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -71,7 +71,7 @@ public class HuffProcessor {
 		out.close();
 	}
 	
-	public HuffNode readTreeHeader(BitInputStream in) {
+	private HuffNode readTreeHeader(BitInputStream in) {
 		int bit = in.readBits(1);
 		if (bit == -1) {
 			throw new HuffException("reading bits failed");
@@ -87,7 +87,7 @@ public class HuffProcessor {
 		}
 	}
 	
-	public void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
+	private void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
 		HuffNode current = root;
 		while (true) {
 			int bits = in.readBits(1);
