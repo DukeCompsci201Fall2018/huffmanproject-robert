d39cec4cb439786d0448d12975840bf1934dfe30
rchen2018 <rec43@duke.edu>
2018-12-07 17:47:58 -0500

    heassss

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -98,7 +98,7 @@ public class HuffProcessor {
 				if (bits == 0) { 
 					current = current.myLeft;
 				}
-				else if(bits == 1) {
+				else{
 					current = current.myRight;
 				}
 				if (current.myLeft == null && current.myRight == null) {
@@ -106,7 +106,7 @@ public class HuffProcessor {
 						break;
 					}
 					else {
-						out.writeBits(1, bits);
+						out.writeBits(5, 223);
 						current = root;
 					}
 				}
